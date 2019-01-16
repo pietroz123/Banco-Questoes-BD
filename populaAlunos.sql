73d60a4e0eddb501030859d7be8e5952896aa8a8
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-01-16 15:25:35 -0200

    Inicio popula nomes

---
+++ populaAlunos.sql
@@ -0,0 +1,21 @@
+
+-- Alunos de BD
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (611685 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (851725 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (592355 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (855907 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (508728 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (907292 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (587888 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (826384 '', '', '', '', '', );
+
+-- Alunos de SO
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (240891 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (111133 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (659187 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (914258 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (319322 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (693596 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (282384 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (282390 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (691045 '', '', '', '', '', );
