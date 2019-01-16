364f89807974933bc1b2ea4c57b130dc910dd6cc
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-01-16 15:34:35 -0200

    Alunos BD

--- populaAlunos.sql
+++ populaAlunos.sql
@@ -1,13 +1,15 @@
 
 -- Alunos de BD
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (611685 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (851725 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (592355 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (855907 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (508728 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (907292 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (587888 '', '', '', '', '', );
-INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (826384 '', '', '', '', '', );
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (611685 'Lucas Oliver Victor Melo', '098.810.918-29', 'lucasolivervictormelo@granvale.com.br', '(11) 3819-0035', '(11) 98958-3398', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (851725 'Vicente Nathan Francisco Mendes', '465.775.178-60', 'vicentenathanfranciscomendes@caferibeiro.com.br', '(11) 2613-9077', '(11) 98630-7733', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (592355 'Larissa Antonella Lara Silveira', '269.560.788-13', 'larissaantonellalarasilveira@ornatopresentes.com.br', '(11) 2505-1593', '(11) 98358-5632', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (855907 'Carlos Eduardo Luan Aparício', '297.720.168-85', 'carloseduardoluanaparicio-76@aulicinobastos.com.br', '(11) 3679-9955', '(11) 99338-7210', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (508728 'Mateus Diego Gomes', '668.047.608-91', 'mateusdiegogomes-73@dillon.com.br', '(11) 2842-6672', '(11) 99806-8291', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (907292 'Gabriela Isabelle Oliveira', '624.785.538-99', 'gabrielaisabelleoliveira@sitran.com.br', '(11) 2987-3428', '(11) 98933-8251', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (587888 'Raquel Luana Freitas', '610.529.428-06', 'rraquelluanafreitas@graffiti.net', '(11) 3713-6269', '(11) 98855-2484', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (826384 'Vitória Maria Oliveira', '732.608.608-78', 'vitoriamariaoliveira@riquefroes.com', '(11) 3699-0358', '(11) 98782-8991', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (587888 'Sérgio Renan Augusto da Cunha', '101.611.928-31', 'sergiorenanaugustodacunha@depotit.com.br', '(11) 2663-3286', '(11) 98844-3240', 11111);
+INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (826384 'Alexandre Benjamin Ian Bernardes', '927.052.178-86', 'aalexandrebenjaminianbernardes@plastic.com.br', '(11) 3620-6334', '(11) 99841-9296', 11111);
 
 -- Alunos de SO
 INSERT INTO aluno (RA_Aluno, Nome_Aluno, CPF, Email_Aluno, Tel_Residencial, Tel_Celular, Codigo_Disciplina) VALUES (240891 '', '', '', '', '', );
