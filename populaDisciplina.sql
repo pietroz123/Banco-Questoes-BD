b5edca707cca823b6bf5ae20ceec04add817b523
Bianca <bgsrd@outlook.com>
2019-01-16 16:28:08 -0200

    Estrutura para inserir nova Disciplina c/ Questões+Alternativas

---
+++ populaDisciplina.sql
@@ -0,0 +1,165 @@
+-- Disciplina de ?
+INSERT INTO disciplina (codigo_disciplina, nome_disciplina, nome_professor, departamento) VALUES (33333, '', '', 'DComp');
+
+
+-- Questões ?
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+INSERT INTO questao (texto_questao, codigo_disciplina) VALUES ('', 33333);
+
+
+-- Alternativas
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 51 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 51 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 51 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 51 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 52 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 52 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 52 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 52 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 53 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 53 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 53 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 53 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 54 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 54 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 54 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 54 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 55 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 55 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 55 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 55 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 56 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 56 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 56 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 56 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 57 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 57 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 57 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 57 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 58 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 58 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 58 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 58 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 59 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 59 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 59 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 59 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 60 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 60 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 60 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 60 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 61 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 61 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 61 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 61 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 62 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 62 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 62 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 62 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 63 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 63 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 63 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 63 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 64 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 64 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 64 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 64 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 65 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 65 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 65 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 65 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 66 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 66 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 66 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 66 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 67 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 67 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 67 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 67 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 68 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 68 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 68 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 68 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 69 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 69 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 69 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 69 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 70 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 70 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 70 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 70 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 71 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 71 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 71 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 71 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 72 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 72 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 72 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 72 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 73 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 73 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 73 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 73 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 74 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 74 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 74 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 74 );
+
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 75 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 75 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 75 );
+INSERT INTO alternativa (Texto_Alternativa, Eh_Correta, ID_Questao) VALUES ('', 'Nao', 75 );
+
+
